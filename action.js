768991645d5127b45f17ca7f4e73f3a43a8ddce2
bs3webdevelop <111056994+bs3webdevelop@users.noreply.github.com>
2023-07-20 00:40:31 +0100

    first site

--- action.js
+++ action.js
@@ -32,4 +32,52 @@ window.addEventListener("DOMContentLoaded", function() {
     // Call the counting function
     countNumbers();
   });
+
+
+     // JavaScript code for the slider
+     const slider = document.querySelector('.slider');
+     const sliderInner = document.querySelector('.slider-inner');
+     const testimonialIndicators = document.querySelectorAll('.testimonial-indicator');
+     
+     let currentSlide = 0;
+     const slideCount = sliderInner.children.length;
+     
+     // Function to move to a specific slide
+     function goToSlide(slideIndex) {
+       currentSlide = slideIndex;
+       updateSlider();
+       updateIndicators();
+     }
+     
+     // Function to move to the next slide
+     function nextSlide() {
+       currentSlide++;
+       if (currentSlide >= slideCount) {
+         currentSlide = 0;
+       }
+       updateSlider();
+       updateIndicators();
+     }
+     
+     // Function to update the slider position
+     function updateSlider() {
+       sliderInner.style.transform = `translateX(-${currentSlide * 100}%)`;
+     }
+     
+     // Function to update the active indicator
+     function updateIndicators() {
+       testimonialIndicators.forEach((indicator, index) => {
+         indicator.classList.toggle('active', index === currentSlide);
+       });
+     }
+     
+     // Event listeners for the testimonial indicators
+     testimonialIndicators.forEach((indicator, index) => {
+       indicator.addEventListener('click', () => {
+         goToSlide(index);
+       });
+     });
+     
+     // Automatic slide transition every 5 seconds
+     setInterval(nextSlide, 5000); /* Updated interval time */
   
